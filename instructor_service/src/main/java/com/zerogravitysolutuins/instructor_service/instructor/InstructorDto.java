f5323f684f818393b9d08cdac08190e8395f192a
Vullnettt <vullnetv389@gmail.com>
2023-03-04 15:57:52 +0100

    instructor-service: DTOs layers implemented

---
+++ instructor_service/src/main/java/com/zerogravitysolutuins/instructor_service/instructor/InstructorDto.java
@@ -0,0 +1,79 @@
+package com.zerogravitysolutuins.instructor_service.instructor;
+
+import com.zerogravitysolutuins.instructor_service.commons.BaseDto;
+
+public class InstructorDto extends BaseDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String bio;
+    private String facebookProfile;
+    private String linkedinProfile;
+    private String profilePicture;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getFacebookProfile() {
+        return facebookProfile;
+    }
+
+    public void setFacebookProfile(String facebookProfile) {
+        this.facebookProfile = facebookProfile;
+    }
+
+    public String getLinkedinProfile() {
+        return linkedinProfile;
+    }
+
+    public void setLinkedinProfile(String linkedinProfile) {
+        this.linkedinProfile = linkedinProfile;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
---
+++ instructor_service/src/main/java/com/zerogravitysolutuins/instructor_service/instructor/utils/InstructorMapper.java
@@ -0,0 +1,81 @@
+package com.zerogravitysolutuins.instructor_service.instructor.utils;
+
+import com.zerogravitysolutuins.instructor_service.instructor.Instructor;
+import com.zerogravitysolutuins.instructor_service.instructor.InstructorDto;
+
+public class InstructorMapper {
+
+    private static Instructor mapDtoToEntity(InstructorDto instructorDto, Instructor instructor){
+
+        if(instructorDto.getId() != null){
+            instructor.setId(instructorDto.getId());
+        }
+        if(instructorDto.getFirstName() != null){
+            instructor.setFirstName(instructorDto.getFirstName());
+        }
+        if(instructorDto.getLastName() != null){
+            instructor.setLastName(instructorDto.getLastName());
+        }
+        if(instructorDto.getEmail() != null){
+            instructor.setEmail(instructorDto.getEmail());
+        }
+        if(instructorDto.getPhoneNumber() != null){
+            instructor.setPhoneNumber(instructorDto.getPhoneNumber());
+        }
+        if(instructorDto.getBio() != null){
+            instructor.setBio(instructorDto.getBio());
+        }
+        if(instructorDto.getFacebookProfile() != null){
+            instructor.setFacebookProfile(instructorDto.getFacebookProfile());
+        }
+        if(instructorDto.getLinkedinProfile() != null){
+            instructor.setLinkedinProfile(instructorDto.getLinkedinProfile());
+        }
+        if(instructorDto.getProfilePicture() != null){
+            instructor.setProfilePicture(instructorDto.getProfilePicture());
+        }
+        if(instructorDto.getCreatedAt() != null){
+            instructor.setCreatedAt(instructorDto.getCreatedAt());
+        }
+        if(instructorDto.getCreatedBy() != null){
+            instructor.setCreatedBy(instructorDto.getCreatedBy());
+        }
+        if(instructorDto.getUpdatedAt() != null){
+            instructor.setUpdatedAt(instructorDto.getUpdatedAt());
+        }
+        if(instructorDto.getUpdatedBy() != null){
+            instructor.setUpdatedBy(instructorDto.getUpdatedBy());
+        }
+        if(instructorDto.getDeletedAt() != null){
+            instructor.setDeletedAt(instructorDto.getDeletedAt());
+        }
+        if(instructorDto.getDeletedBy() != null){
+            instructor.setDeletedBy(instructorDto.getDeletedBy());
+        }
+        return instructor;
+    }
+
+    public static InstructorDto mapEntityToDto(Instructor instructor) {
+
+        InstructorDto instructorDto = new InstructorDto();
+
+        instructorDto.setId(instructor.getId());
+        instructorDto.setFirstName(instructor.getFirstName());
+        instructorDto.setLastName(instructor.getLastName());
+        instructorDto.setEmail(instructor.getEmail());
+        instructorDto.setPhoneNumber(instructor.getPhoneNumber());
+        instructorDto.setBio(instructor.getBio());
+        instructorDto.setFacebookProfile(instructor.getFacebookProfile());
+        instructorDto.setLinkedinProfile(instructor.getLinkedinProfile());
+        instructorDto.setProfilePicture(instructor.getProfilePicture());
+
+        instructorDto.setCreatedAt(instructor.getCreatedAt());
+        instructorDto.setCreatedBy(instructor.getCreatedBy());
+        instructorDto.setUpdatedAt(instructor.getUpdatedAt());
+        instructorDto.setUpdatedBy(instructor.getUpdatedBy());
+        instructorDto.setDeletedAt(instructor.getDeletedAt());
+        instructorDto.setDeletedBy(instructor.getDeletedBy());
+
+        return instructorDto;
+    }
+}
