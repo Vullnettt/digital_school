cd63918b950dfc13209ef72b612c101b02e59e02
Vullnettt <vullnetv389@gmail.com>
2023-03-06 17:26:07 +0100

    group-service data-transfer-object and group mapper implemented

---
+++ group-service/src/main/java/com/zerogravitysolutions/groupservice/group/GroupDto.java
@@ -0,0 +1,46 @@
+package com.zerogravitysolutions.groupservice.group;
+
+import com.zerogravitysolutions.groupservice.commons.BaseDto;
+
+import java.sql.Timestamp;
+
+public class GroupDto extends BaseDto{
+
+    private String title;
+    private String description;
+    private Timestamp startDate;
+    private Timestamp endDate;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+}
---
+++ group-service/src/main/java/com/zerogravitysolutions/groupservice/group/utils/GroupMapper.java
@@ -0,0 +1,66 @@
+package com.zerogravitysolutions.groupservice.group.utils;
+
+import com.zerogravitysolutions.groupservice.group.Group;
+import com.zerogravitysolutions.groupservice.group.GroupDto;
+
+public class GroupMapper {
+
+    public static Group mapDtoToEntity(GroupDto groupDto, Group group){
+
+        if(groupDto.getId() != null){
+            group.setId(groupDto.getId());
+        }
+        if(groupDto.getTitle() != null){
+            group.setTitle(groupDto.getTitle());
+        }
+        if(groupDto.getDescription() != null){
+            group.setDescription(groupDto.getDescription());
+        }
+        if(groupDto.getStartDate() != null){
+            group.setStartDate(groupDto.getStartDate());
+        }
+        if(groupDto.getEndDate() != null){
+            group.setEndDate(groupDto.getEndDate());
+        }
+
+        if(groupDto.getCreatedAt() != null){
+            group.setCreatedAt(groupDto.getCreatedAt());
+        }
+        if(groupDto.getCreatedBy() != null){
+            group.setCreatedBy(groupDto.getCreatedBy());
+        }
+        if(groupDto.getUpdatedAt() != null){
+            group.setUpdatedAt(groupDto.getUpdatedAt());
+        }
+        if(groupDto.getUpdatedBy() != null){
+            group.setUpdatedBy(groupDto.getUpdatedBy());
+        }
+        if(groupDto.getDeletedAt() != null){
+            group.setDeletedAt(groupDto.getDeletedAt());
+        }
+        if(groupDto.getDeletedBy() != null){
+            group.setDeletedBy(groupDto.getDeletedBy());
+        }
+        return group;
+    }
+
+    public static GroupDto mapEntityToDto(Group group) {
+
+        GroupDto groupDto = new GroupDto();
+
+        groupDto.setId(group.getId());
+        groupDto.setTitle(group.getTitle());
+        groupDto.setDescription(group.getDescription());
+        groupDto.setStartDate(group.getStartDate());
+        groupDto.setEndDate(group.getEndDate());
+
+        groupDto.setCreatedAt(group.getCreatedAt());
+        groupDto.setCreatedBy(group.getCreatedBy());
+        groupDto.setUpdatedAt(group.getUpdatedAt());
+        groupDto.setUpdatedBy(group.getUpdatedBy());
+        groupDto.setDeletedAt(group.getDeletedAt());
+        groupDto.setDeletedBy(group.getDeletedBy());
+
+        return groupDto;
+    }
+}
