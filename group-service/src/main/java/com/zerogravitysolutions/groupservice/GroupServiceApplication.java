86d068901322d6b5d6a7d7e9994556ad834aff74
Vullnettt <vullnetv389@gmail.com>
2023-03-10 14:31:42 +0100

    discovery-service implemented and connected with all services

--- group-service/src/main/java/com/zerogravitysolutions/groupservice/GroupServiceApplication.java
+++ group-service/src/main/java/com/zerogravitysolutions/groupservice/GroupServiceApplication.java
@@ -2,8 +2,10 @@ package com.zerogravitysolutions.groupservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GroupServiceApplication {
 
 	public static void main(String[] args) {
--- instructor_service/src/main/java/com/zerogravitysolutuins/instructor_service/InstructorServiceApplication.java
+++ instructor_service/src/main/java/com/zerogravitysolutuins/instructor_service/InstructorServiceApplication.java
@@ -2,8 +2,10 @@ package com.zerogravitysolutuins.instructor_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InstructorServiceApplication {
 
 	public static void main(String[] args) {
